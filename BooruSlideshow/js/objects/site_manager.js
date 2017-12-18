f318c028ea5ca4485b363705bbf03f3fa693c4de
Drew Arrigoni <arrigoni.andrew@gmail.com>
2017-12-18 16:10:26 -0500

    MP4 Support

--- BooruSlideshow/js/objects/site_manager.js
+++ BooruSlideshow/js/objects/site_manager.js
@@ -552,10 +552,12 @@ SiteManager.prototype = {
 	getMediaTypeFromPath: function (filePath)
 	{
 		var fileExtension = filePath.substring(filePath.length - 4);
-		
+
+		console.log(fileExtension.toLowerCase());
 		switch (fileExtension.toLowerCase())
 		{
 			case 'webm':
+			case '.mp4':
 				return MEDIA_TYPE_VIDEO;
 			case '.gif':
 				return MEDIA_TYPE_GIF;
