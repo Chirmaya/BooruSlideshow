66d7ed99ddcbc1dcfeff7941ca02f7add0299349
Not Me <extras@is-just-a.dev>
2020-06-02 13:01:15 -0400

    Forgot to repack

--- BooruSlideshow/js/objects/site_managers/site_manager_derpibooru.js
+++ BooruSlideshow/js/objects/site_managers/site_manager_derpibooru.js
@@ -1,41 +1,33 @@
-class SiteManagerDerpibooru extends SiteManager
-{
-    constructor(sitesManager, pageLimit)
-    {
+class SiteManagerDerpibooru extends SiteManager {
+	constructor(sitesManager, pageLimit) {
 		super(sitesManager, SITE_DERPIBOORU, 'https://derpibooru.org', pageLimit);
-    }
-    
-    buildPingRequestUrl()
-	{
+	}
+
+	buildPingRequestUrl() {
 		return this.url + '/api/v1/json/images/1';
-    }
-    
-    buildRequestUrl(searchText, pageNumber)
-	{
+	}
+
+	buildRequestUrl(searchText, pageNumber) {
 		var query = this.buildSiteSpecificQuery(searchText);
-		
+
 		var possibleAddedKey = this.sitesManager.model.derpibooruApiKey ? '&key=' + this.sitesManager.model.derpibooruApiKey : '';
-		
+
 		return this.url + '/api/v1/json/search/images?q=' + this.prepareQueryForDerpibooru(query) + '&page=' + pageNumber + '&limit=' + this.pageLimit + possibleAddedKey;
 	}
 
-	prepareQueryForDerpibooru(searchQuery)
-    {
-        searchQuery = this.addCommasToSearchQuery(searchQuery);
-        return this.replaceUnderscoresWithSpaces(searchQuery);
+	prepareQueryForDerpibooru(searchQuery) {
+		searchQuery = this.addCommasToSearchQuery(searchQuery);
+		return this.replaceUnderscoresWithSpaces(searchQuery);
 	}
-	
-	addCommasToSearchQuery(searchQuery)
-	{
+
+	addCommasToSearchQuery(searchQuery) {
 		return searchQuery.replace(" ", ",");
 	}
 
-	replaceUnderscoresWithSpaces(searchQuery)
-	{
+	replaceUnderscoresWithSpaces(searchQuery) {
 		let queryItems = searchQuery.split(",");
 
-		for (let i = 0; i < queryItems.length; i++)
-		{
+		for (let i = 0; i < queryItems.length; i++) {
 			let queryItem = queryItems[i];
 
 			if (this.startWith(queryItem, 'aspect_ratio'))
@@ -55,63 +47,55 @@ class SiteManagerDerpibooru extends SiteManager
 
 			queryItems[i] = queryItem.replace("_", " ");
 		}
-		
+
 		return queryItems.join(',');
 	}
 
-	startWith(text, term)
-	{
-		return (text.substring(0,term.length) == term);
+	startWith(text, term) {
+		return (text.substring(0, term.length) == term);
 	}
 
-	doesResponseTextIndicateOnline(responseText)
-	{
+	doesResponseTextIndicateOnline(responseText) {
 		var jsonPosts;
-		
-		try
-		{
+
+		try {
 			jsonPosts = JSON.parse(responseText);
 		}
-		catch(e)
-		{
+		catch (e) {
 			console.log("JSON failed to parse.");
 			console.log(e);
 			return false;
 		}
-		
+
 		// Derpibooru-only line
 		jsonPosts = jsonPosts["image"];
-		
+
 		if (jsonPosts == null)
 			return false;
 		
 		return true;
 	}
 
-	addSlides(responseText)
-	{
+	addSlides(responseText) {
 		this.addJsonSlides(responseText);
 	}
 
-	addSlide(jsonPost)
-	{
-		if (jsonPost.hasOwnProperty('representations'))
-		{
-			if (this.isPathForSupportedMediaType(jsonPost.representations["medium"]))
-			{
+	addSlide(jsonPost) {
+		if (jsonPost.hasOwnProperty('representations')) {
+			if (this.isPathForSupportedMediaType(jsonPost.representations["medium"])) {
 				var tags = jsonPost.tags.join(",");
-				
-				tags = tags.replace(/,\s/gm,",");
-				tags = tags.replace(/\s/gm,"_");
-				tags = tags.replace(/,/gm," ");
-				
+
+				tags = tags.replace(/,\s/gm, ",");
+				tags = tags.replace(/\s/gm, "_");
+				tags = tags.replace(/,/gm, " ");
+
 				if (this.areSomeTagsAreBlacklisted(tags))
 					return;
 				jsonPost.rating = jsonPost.tags.includes("explicit") ? "e" : jsonPost.tags.includes("suggestive") || jsonPost.tags.includes("questionable") ? "q" : "s"
-				if(!this.isRatingAllowed(jsonPost.rating))
+				if (!this.isRatingAllowed(jsonPost.rating))
 					return
 				// console.log(jsonPost)
-				
+
 				var newSlide = new Slide(
 					SITE_DERPIBOORU,
 					jsonPost.id,
